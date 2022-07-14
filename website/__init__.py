d3b93e890375da08134eee856fa7d386959a2d46
perezchris3599 <kweeziperez712@gmail.com>
2022-07-14 18:23:24 +0300

    created basic flask app

---
+++ website/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = "helloworld"
+
+    return app
+    
