5432cf3dae6bbdb7a539330c3e996ad7c7cb6855
perezchris3599 <kweeziperez712@gmail.com>
2022-07-14 19:47:48 +0300

    rendered templates

--- website/views.py
+++ website/views.py
@@ -3,5 +3,6 @@ from flask import Blueprint, render_template
 views = Blueprint("views", __name__)
 
 @views.route("/")
+@views.route("/home")
 def home():
-    return "Home"
+    return render_template("home.html")
