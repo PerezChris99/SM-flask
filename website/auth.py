4cb5a5e9e922a753575144df65277c21f3782b67
perezchris3599 <kweeziperez712@gmail.com>
2022-07-14 20:58:01 +0300

    added navbar to the pages

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,15 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 
 auth = Blueprint("auth", __name__)
 
 @auth.route("/login")
 def login():
-    return "Login"
+    return render_template("login.html")
 
-@auth.route("/sign-up")
-def sign_up():
-    return "Sign-up"
+@auth.route("/signup")
+def signup():
+    return render_template("signup.html")
 
 @auth.route("/logout")
 def logout():
-    return "Logout"
+    return redirect(url_for("views.home"))
