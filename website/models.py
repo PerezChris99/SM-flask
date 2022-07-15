e43d33421b5b2c01ba98d4609f7b03d301fe9398
perezchris3599 <kweeziperez712@gmail.com>
2022-07-15 20:12:12 +0300

    added relationship

--- website/models.py
+++ website/models.py
@@ -9,10 +9,11 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    posts = db.relationship('Post', backref='user', passive_deletes=True)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.Text, nullable=False)
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
-    
+
