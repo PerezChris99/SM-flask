660be82596806461aeb6e61ddd1176b5cc3d8d78
perezchris3599 <kweeziperez712@gmail.com>
2022-07-15 13:57:58 +0300

    login manager

--- website/__init__.py
+++ website/__init__.py
@@ -18,7 +18,17 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
+    from .models import User
+
     create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
     
