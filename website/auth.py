8201cd16ee1a9f7d86807c898988e952308798f6
perezchris3599 <kweeziperez712@gmail.com>
2022-07-15 13:21:48 +0300

    security

--- website/auth.py
+++ website/auth.py
@@ -2,12 +2,19 @@ from flask import Blueprint, render_template, redirect, url_for
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods=['GET', 'POST'])
 def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
     return render_template("login.html")
 
-@auth.route("/signup")
+@auth.route("/signup", methods=['GET', 'POST'])
 def signup():
+    email = request.form.get("email")
+    username = request.form.get("userame")
+    password1 = request.form.get("password1")
+    password2 = request.form.get("password2")
+
     return render_template("signup.html")
 
 @auth.route("/logout")
