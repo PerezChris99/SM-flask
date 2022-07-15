0bffe619b433cb436ce15e9c44f047e53ded80e0
perezchris3599 <kweeziperez712@gmail.com>
2022-07-15 19:40:58 +0300

    make a post

--- website/views.py
+++ website/views.py
@@ -9,3 +9,8 @@ views = Blueprint("views", __name__)
 @login_required
 def home():
     return render_template("home.html", user=current_user)
+
+@views.route("/create-post", methods=['GET', 'POST'])
+@login_required
+def create_post():
+    return render_template('create_post.html', user=current_user)
