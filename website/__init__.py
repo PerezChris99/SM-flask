e022cec8035089b36d8ae2b9aab89d6df9318ab2
perezchris3599 <kweeziperez712@gmail.com>
2022-07-15 18:33:02 +0300

    creating the user model

--- website/__init__.py
+++ website/__init__.py
@@ -6,6 +6,7 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "helloworld"
@@ -31,7 +32,7 @@ def create_app():
         return User.query.get(int(id))
 
     return app
-    
+
 def create_database(app):
     if not path.exists("website/" + DB_NAME):
         db.create_all(app=app)
