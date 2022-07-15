c01a83bd09c5ab1917ebf4c25604fcaefc0cf654
perezchris3599 <kweeziperez712@gmail.com>
2022-07-15 19:26:27 +0300

    added permanent database

--- website/auth.py
+++ website/auth.py
@@ -23,7 +23,7 @@ def login():
         else:
             flash('Email does not exist.', category='error')
 
-    return render_template("login.html")
+    return render_template("login.html", user=current_user)
 
 @auth.route("/sign-up", methods=['GET', 'POST'])
 def sign_up():
@@ -57,7 +57,7 @@ def sign_up():
             flash('User created!')
             return redirect(url_for('views.home'))
 
-    return render_template("signup.html")
+    return render_template("signup.html", user=current_user)
 
 @auth.route("/logout")
 @login_required
