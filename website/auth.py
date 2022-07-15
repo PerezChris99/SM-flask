56615bf5b945e1944771e1d53ffb9ce5430fa12b
perezchris3599 <kweeziperez712@gmail.com>
2022-07-15 15:00:11 +0300

    password hash

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,13 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from . import db
+from models import User
+from flask_login import login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 auth = Blueprint("auth", __name__)
+
+
 
 @auth.route("/login", methods=['GET', 'POST'])
 def login():
@@ -10,10 +17,34 @@ def login():
 
 @auth.route("/signup", methods=['GET', 'POST'])
 def signup():
-    email = request.form.get("email")
-    username = request.form.get("userame")
-    password1 = request.form.get("password1")
-    password2 = request.form.get("password2")
+    if request.method == 'POST':
+        email = request.form.get("email")
+        username = request.form.get("userame")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        email_exists = User.query.filter_by(email=email).first()
+        username_exists = User.query.filter_by(username=username).first()
+        
+        if email_exists:
+            flash('Email is already in use.', category='error')
+        elif username_exists:
+            flash('Username is already in use.', category='error')
+        elif password1 != password2:
+            flash('Password don\'t match!', category='error')
+        elif len(username) < 2:
+            flash('Username is too short.', category='error')
+        elif len(password1) < 6:
+            flash('Password is too short.', category='error')
+        elif len(email) < 4:
+            flash("Email is invalid.", category='error')
+        else:
+            new_user = User(email=email, username=username, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('User created!')
+            return redirect(url_for('views.home'))
+
 
     return render_template("signup.html")
 
