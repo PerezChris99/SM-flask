27a71d99aa07648a5b96a4f4dfedfc4a79080351
perezchris3599 <kweeziperez712@gmail.com>
2022-07-16 14:58:21 +0300

    posting comments to db

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Post, User
+from .models import Post, User, Comment
 from . import db
 
 views = Blueprint("views", __name__)
@@ -58,5 +58,19 @@ def posts(username):
     posts = user.posts
     return render_template("posts.html", user=current_user, posts=posts, username=username)
 
+@views.route("/create-comment/<post_id>")
+def create_comment(post_id):
+    text = request.form.get('text')
 
+    if not text:
+        flash('Comment cannot be empty.', category='error')
+    else:
+        post = Post.query.filter_by(id=post_id)
+        if post:
+            comment = Comment(text=text, author=current_user.id, post_id=post_id)
+            db.session.add(comment)
+            db.session.commit()
+        else:
+            flash('post does not exist.', category='error')
 
+    return redirect(url_for('views.home'))
