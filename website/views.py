4119dd8a32e00d739351ea7ca76656966b591691
perezchris3599 <kweeziperez712@gmail.com>
2022-07-16 13:24:25 +0300

    comment class and relationships

--- website/views.py
+++ website/views.py
@@ -55,7 +55,7 @@ def posts(username):
         flash('No user with that username exists.', category='error')
         return redirect(url_for('views.home'))
 
-    posts = Post.query.filter_by(author=user.id).all()
+    posts = user.posts
     return render_template("posts.html", user=current_user, posts=posts, username=username)
 
 
