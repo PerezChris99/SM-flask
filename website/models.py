c2e543f805dcb488e3e55ddc56ef0debc9299a38
perezchris3599 <kweeziperez712@gmail.com>
2022-07-16 19:28:33 +0300

    thumbs up button

--- website/models.py
+++ website/models.py
@@ -32,5 +32,6 @@ class Like(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
-    post_id = db.Column(bd.Integer, db.ForeignKey('post.id', ondelete="CASCADE"), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete="CASCADE"), nullable=False)
+
 
